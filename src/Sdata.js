8be6fdd506645d87926fa58a195e29e445f59554
nannu08 <nandaninannu@example.com>
2021-07-19 19:00:09 +0530

    First commit

---
+++ src/Sdata.js
@@ -0,0 +1,43 @@
+const Sdata= [
+    {
+        sname:"Hasena Dilruba",
+        imgsrc: "https://occ-0-2611-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABYuGzLe962ogE9Ek4mE3-WphgWZgPyNAwC8H-6SeET-zQc5GlOgo9_L_YbSCbOLq54B56fvjBE5CF6XN8EJ5PyS-oNupK0gE9O8xEcjUpGOKTEc9PWG2jp7pZ6LB.jpg?r=f8a",
+        title:"Netflix Original series",
+        links: "https://www.netflix.com/in/title/81320770",
+    },
+    {
+        sname:"Zindgi Na Milegi Dobara",
+        imgsrc: "https://occ-0-2611-3663.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU43-s_7zW7uZPVDfjeqPWDZo-Wu5LuOkIEHpZIEJZ4AtT9aWkw2-ivEyc-5iJQIvAFhcgpuaHdklsyoWRzJIhjXEqVjWVI46pXPRVB3kpCLXFTZaAvah2d9GbDx.jpg?r=20e",
+        title:"Netflix Original series",
+        links: "https://www.netflix.com/in/title/81438809",
+    },
+    {
+        sname:"Jagme",
+        imgsrc: "https://occ-0-2611-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQKtD_r5rtoOav3NGYAQg0KGA6VHPMjEBz0jbSbruR3sqoAooGlTRjJ2gUeyBp_urlp3wGL-TAQxYAoyU4npXvUjxYVN9h73yArmReZelInAcpXJp1cH2itwe_pL.jpg?r=023",
+        title:"Netflix Original series",
+        links: "https://www.netflix.com/in/title/81417659",
+    },
+    {
+        sname:"Swadesh",
+        imgsrc: "https://occ-0-2611-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTW9z3JNpCl0xVTfx3oSKkp7GMlwxrf7f0OteeU3cinoPEnr5iVvNN3ONHG7ncSbTFqSsQZEOmrsFFw4JJiSj8pYFUc.jpg?r=bea",
+        title:"Netflix Original series",
+        links: "https://www.netflix.com/in/title/70019491",
+    },
+    {
+        sname:"Gunjan Saxena",
+        imgsrc: "https://occ-0-2611-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABcX5j_Xc603VcZl6B8JD1TmSB9p1wjw6nqU4qpdmXioCdsSGEy1ufhb0PZneUJ2pQH6IC8KXpuVrmV_CHDhkM477dugrLa2J-5ly_4BLmu_XUadoVgrosvicAvdw.jpg?r=173",
+        title:"Netflix Original series",
+        links: "https://www.netflix.com/in/title/81292944",
+    },
+    {
+        sname:"Jagme",
+        imgsrc: "https://occ-0-2611-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQKtD_r5rtoOav3NGYAQg0KGA6VHPMjEBz0jbSbruR3sqoAooGlTRjJ2gUeyBp_urlp3wGL-TAQxYAoyU4npXvUjxYVN9h73yArmReZelInAcpXJp1cH2itwe_pL.jpg?r=023",
+        title:"Netflix Original series",
+        links: "https://www.netflix.com/in/title/81417659",
+    },
+    
+
+
+
+];
+export default Sdata;
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Card from './Cards';
+import Sdata from './Sdata';
+
+
+
+ReactDOM.render(
+  <>
+  
+    <h1 className="heading_style" >List of top Netflix Series</h1>
+
+ { Sdata.map((val,index)=>{
+   console.log(index);
+   return(
+    <Card imgsrc={val.imgsrc}
+    title={val.title}
+    sname={val.sname}
+    link={val.link} />
+
+
+  );
+
+ })}
+     
+  </>,
+  document.getElementById('root')
+);
+
